46c972a77bdf39f7cbdce08e6c672db2e9350aa2
saineha2000 <81003565+saineha2000@users.noreply.github.com>
2022-04-27 11:34:29 +0530

    Add files via upload

--- src/neha/Report-project/react-material-ui-master/react-material-ui-master/src/Components/Report/Col3.js
+++ src/neha/Report-project/react-material-ui-master/react-material-ui-master/src/Components/Report/Col3.js
@@ -22,18 +22,42 @@ import { DatePicker } from "@mui/lab";
 import FileDownload from "js-file-download";
 import { initialValues } from "../GlobalData/Storereport";
 import axios from "axios";
-import theme from "../Theme"
+import theme from "../Theme";
 
 import { makeStyles } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 
 import { ThemeProvider } from "@material-ui/core/styles";
+import PickersDay, {
+  PickersDayProps,
+  pickersDayClasses,
+} from "@mui/lab/PickersDay";
 export const Col3 = () => {
   const [state1, setState1] = useContext(Context1);
 
   const [open, setOpen] = React.useState(false);
   // const theme = useTheme();
   //const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  const renderWeekPickerDay = (
+    date: Date,
+    selectedDates: Array<Date | null>,
+    pickersDayProps: PickersDayProps<Date>
+    // date: "",
+    // selectedDates: [],
+    // pickersDayProps: PickersDayProps<Date>
+  ) => {
+    return (
+      <PickersDay
+        {...pickersDayProps}
+        sx={{
+          [`&&.${pickersDayClasses.selected}`]: {
+            backgroundColor: "#5514B4",
+          },
+        }}
+      />
+    );
+  };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -47,11 +71,10 @@ export const Col3 = () => {
       "& .MuiTypography-root": {
         width: "auto",
       },
-     " .css-bkrceb-MuiButtonBase-root-MuiPickersDay-root.Mui-selected":{
-            backgroundColor:"#5514B4",
-      }
+      " .css-bkrceb-MuiButtonBase-root-MuiPickersDay-root.Mui-selected": {
+        backgroundColor: "#5514B4",
+      },
     },
-     
   }));
   const handleClickOpen = () => {
     setOpen(true);
@@ -62,7 +85,7 @@ export const Col3 = () => {
   };
   const downloadCsv = () => {
     // e.preventDefault();
-    //setOpen(true);
+    setOpen(true);
     //   axios({
     //     url: "http://localhost:8083",
     //     method: "GET",
@@ -89,13 +112,17 @@ export const Col3 = () => {
       });
     setState1(initialValues);
   };
+
   const classes = useStyle();
   return (
     <ThemeProvider theme={theme}>
       <Grid container>
         <Grid item lg={3} md={3} sm={12} xs={12} className={classes.rootClass}>
           <br />
-          <Typography align="left"  style={{ marginLeft: "24px",fontWeight:"bold" }}>
+          <Typography
+            align="left"
+            style={{ marginLeft: "24px", fontWeight: "bold" }}
+          >
             Product Type
           </Typography>
           <Box sx={{ display: "flex", flexDirection: "column", ml: 3 }}>
@@ -107,7 +134,7 @@ export const Col3 = () => {
                 control={
                   <Checkbox
                     name="product_type_n_i_col1"
-                    style={{color:"#5514B4"}}
+                    style={{ color: "#5514B4" }}
                     checked={state1.product_type_n_i_col1 ? true : false}
                     // style={{ paddingLeft: "10px" }}
                   />
@@ -120,8 +147,8 @@ export const Col3 = () => {
                 labelPlacement="end"
                 control={
                   <Checkbox
-                  color={"primary"}
-                     //style={{color:"#5514B4"}}
+                    color={"primary"}
+                    //style={{color:"#5514B4"}}
                     name="product_type_i_i_col1"
                     checked={state1.product_type_i_i_col1 ? true : false}
                   />
@@ -134,7 +161,10 @@ export const Col3 = () => {
         <Grid item lg={4} md={4} sm={12} xs={12} className={classes.rootClass}>
           <br />
           {/* <InputLabel>Additional Parameters</InputLabel> */}
-          <Typography align="left"  style={{ marginLeft: "24px",fontWeight:"bold" }}>
+          <Typography
+            align="left"
+            style={{ marginLeft: "24px", fontWeight: "bold" }}
+          >
             Additional Parameters
           </Typography>
           <Box sx={{ display: "flex", flexDirection: "column", ml: 3 }}>
@@ -144,7 +174,7 @@ export const Col3 = () => {
                 labelPlacement="end"
                 control={
                   <Checkbox
-                   style={{color:"#5514B4"}}
+                    style={{ color: "#5514B4" }}
                     name="show_external_code"
                     checked={state1.show_external_code ? true : false}
                   />
@@ -156,7 +186,7 @@ export const Col3 = () => {
                 labelPlacement="end"
                 control={
                   <Checkbox
-                   style={{color:"#5514B4"}}
+                    style={{ color: "#5514B4" }}
                     name="sms_service"
                     checked={state1.sms_service ? true : false}
                   />
@@ -169,7 +199,10 @@ export const Col3 = () => {
         </Grid>
         <Grid item lg={5} md={5} sm={12} xs={12} className={classes.rootClass}>
           <br />
-          <Typography align="left"    style={{ marginLeft: "24px",fontWeight:"bold" }}>
+          <Typography
+            align="left"
+            style={{ marginLeft: "24px", fontWeight: "bold" }}
+          >
             Gaining Suppliers
           </Typography>
           <Box sx={{ display: "flex", flexDirection: "column", ml: 3 }}>
@@ -178,7 +211,7 @@ export const Col3 = () => {
                 // sx={{ m: 1 }}
                 control={
                   <Checkbox
-                   style={{color:"#5514B4"}}
+                    style={{ color: "#5514B4" }}
                     name="show_dialed_access_no"
                     checked={state1.show_dialed_access_no ? true : false}
                   />
@@ -190,7 +223,7 @@ export const Col3 = () => {
                 // sx={{ m: 1 }}
                 control={
                   <Checkbox
-                   style={{color:"#5514B4"}}
+                    style={{ color: "#5514B4" }}
                     name="show_network_address_suppliers"
                     checked={
                       state1.show_network_address_suppliers ? true : false
@@ -203,12 +236,14 @@ export const Col3 = () => {
             </FormGroup>
           </Box>
         </Grid>
-        <Grid container >
+        <Grid container>
           <Grid item lg={6} md={6} sm={12} xs={12}>
             <br />
             <label>Date created From :</label>
-            <DatePicker 
-            className={classes.rootClass}
+            <DatePicker
+              // className={classes.DatePicker}
+              renderDay={renderWeekPickerDay}
+              //className={classes.rootClass}
               value={state1.date_created_from}
               onChange={(newValue) => {
                 setState1({
@@ -217,7 +252,7 @@ export const Col3 = () => {
                 });
               }}
               renderInput={(params) => (
-                <TextField name="date_created_from" {...params} />
+                <TextField name="date_created_from" sx={{}} {...params} />
               )}
             />
           </Grid>
@@ -226,7 +261,7 @@ export const Col3 = () => {
             <br />
             <label>Date created To :</label>
             <DatePicker
-              style={{backgroundColor:"#5514B4"}}
+              renderDay={renderWeekPickerDay}
               value={state1.date_created_to}
               onChange={(newValue) => {
                 setState1({
@@ -235,14 +270,14 @@ export const Col3 = () => {
                 });
               }}
               renderInput={(params) => (
-                <TextField name="date_created_to"{...params} />
+                <TextField name="date_created_to" {...params} />
               )}
             />
           </Grid>
           <Grid container item justifyContent={"center"}>
             <Button
               // align="center"
-              sx={{ m: 4,backgroundColor:"#5514B4" }}
+              sx={{ m: 4, backgroundColor: "#5514B4" }}
               variant="contained"
               onClick={
                 // ()=>{
@@ -253,9 +288,31 @@ export const Col3 = () => {
             >
               submit
             </Button>
-           
-         
-        
+            <Dialog
+              // fullScreen={fullScreen}
+              open={open}
+              aria-labelledby="responsive-dialog-title"
+            >
+              <DialogTitle id="responsive-dialog-title">
+                {"Downloading in progress..."}
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText>please wait...</DialogContentText>
+              </DialogContent>
+            </Dialog>
+            {/* <Button onClick={downloadCsv}>download</Button>
+          <Dialog
+            // fullScreen={fullScreen}
+            open={open}
+            aria-labelledby="responsive-dialog-title"
+          >
+            <DialogTitle id="responsive-dialog-title">
+              {"Downloading in progress..."}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText>please wait...</DialogContentText>
+            </DialogContent>
+          </Dialog> */}
           </Grid>
         </Grid>
       </Grid>
