c8cc311ba34b4e28c6ada4804e1580f3ada9eafd
saineha2000 <81003565+saineha2000@users.noreply.github.com>
2022-05-07 11:04:04 +0530

    Add files via upload

--- src/Components/Theme.js
+++ src/Components/Theme.js
@@ -1,21 +1,29 @@
-import { createTheme} from '@material-ui/core/styles';
+import { createTheme } from "@material-ui/core/styles";
 
 const theme = createTheme({
-    // palette: {
-    //   primary : {
-    //     main : '#6A359C',
-    //     light : '#e7feff'
+  // palette: {
+  //   primary : {
+  //     main : '#6A359C',
+  //     light : '#e7feff'
+  palette: {
+    primary: { main: "#5514B4" },
+    secondary: { main: "#4d5151" },
+  },
 
+  // typography: {
+  //     fontFamily: [
+  //       "BT Font Regular", "Calibri", "Arial", "sans-serif"
+  //     ].join(",")
+  //   }
 
-        
-    //   },
-    //   secondary: {
-    //       //Orange
-    //     light: '#ff7961',
-    //     main: '#f44336',
-    //     dark: '#ba000d',
-    //   },
-    // },
-  });
+  //   },
+  //   secondary: {
+  //       //Orange
+  //     light: '#ff7961',
+  //     main: '#f44336',
+  //     dark: '#ba000d',
+  //   },
+  // },
+});
 
-export default theme;  
+export default theme;
