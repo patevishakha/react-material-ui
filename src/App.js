7d3dbf48ac53ff0c4c020c16c12b5af7539ea8c7
Vishakha Sudhir Pate <55608888+patevishakha@users.noreply.github.com>
2022-06-03 12:29:25 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
+import React, { useEffect } from "react";
 import { CssBaseline } from "@material-ui/core";
 import "./App.css";
 import Store from "./Components/GlobalData/Store";
+import Favourites from "./Components/GlobalData/Favourites";
 
 import { makeStyles } from "@material-ui/core/styles";
+import { BrowserRouter} from "react-router-dom";
 import Persistant from "./Components/Persistant";
+
 import Storereport from "./Components/GlobalData/Storereport";
 import { LocalizationProvider } from "@mui/lab";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
+
+
+
 const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
   },
+ 
 }));
 
 function App() {
   const classes = useStyles();
+
   return (
+    <BrowserRouter>
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <Store>
         <Storereport>
-          <div className="App">
+          <Favourites>
+          {/* <div className={classes.toolbarMargin} /> */}
             <CssBaseline />
+            <div className={classes.toolbarMargin} />
             <Persistant />
+            
+           
             {/* <Header/> */}
             <div className={classes.toolbarMargin} />
-          </div>
+          </Favourites>
         </Storereport>
       </Store>
     </LocalizationProvider>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { usePromiseTracker } from "react-promise-tracker";
+import * as Loader from "react-loader-spinner";
+   /*export const Loader = () => (
+  <div className="flex justify-center items-center ">
+    <ThreeDots color="#2BAD60" height={80} width={80} />
+  </div>
+); */
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -11,7 +18,32 @@ import reportWebVitals from './reportWebVitals';
 //   document.getElementById('root')
 // );
 
-ReactDOM.render(<App />,document.getElementById('root'));
+   const LoadingIndicator = props => {
+   const { promiseInProgress } = usePromiseTracker();
+   
+
+
+   return (
+    promiseInProgress && 
+        <div
+      style={{
+	    position: 'fixed',
+		left: 0,
+		top: 300,
+        width: "100%",
+        height: "100",
+		zIndex: "9999",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Loader.ThreeDots color= "#5514B4"/>
+    </div>
+  );  
+ }
+
+ReactDOM.render(<div> <LoadingIndicator /> <App /> </div>  ,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
